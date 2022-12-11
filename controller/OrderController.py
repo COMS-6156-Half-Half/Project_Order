392182b94d670f17f9a3c6428a278361bb5e9aa1
xinyu-7 <84774087+xinyu-7@users.noreply.github.com>
2022-12-11 17:26:28 -0500

    Update OrderController.py

--- controller/OrderController.py
+++ controller/OrderController.py
@@ -2,12 +2,15 @@ from flask import Flask, Response, request
 import json
 from OrderDB import OrderDB
 
+
 # Create the Flask application object.
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
 
 
 @app.route("/api/record/all", methods=["GET"])
@@ -22,14 +25,13 @@ def get_all_record():
 
     return rsp
 
-@app.route("/api/record/insert/", methods=["POST"])
+@app.route("/api/record/insert", methods=["POST"])
 def insert_record():
-
-    id = int(request.args.get("id"))
-    seller_id = int(request.args.get("seller_id"))
-    product_id = int(request.args.get("product_id"))
-    buyer_id = int(request.args.get("buyer_id"))
-
+    data = request.get_json()
+    id =int(data["id"])
+    seller_id = int(data["seller_id"])
+    product_id = int(data["product_id"])
+    buyer_id = int(data["buyer_id"])
     result = OrderDB.insert_record(id, seller_id, product_id, buyer_id)
 
     if result:
@@ -65,7 +67,8 @@ def get_by_buyerId(buyer_id):
 
 @app.route("/api/record/delete/", methods=["POST"])
 def delete_by_orderId():
-    order_id = int(request.args.get("order_id"))
+    data = request.get_json()
+    order_id = int(data["order_id"])
     result = OrderDB.delete_by_orderId(order_id)
 
     if result:
